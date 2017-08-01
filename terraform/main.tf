f581549ba4a7cb8b1610a2081503c825492e37d7
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-08-01 22:23:44 +0100

    WIP on Lambda Function

---
+++ terraform/main.tf
@@ -0,0 +1,79 @@
+variable project {
+  default = "borked"
+}
+
+data "aws_caller_identity" "current" {}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "charlieegan3-www-terraform-state"
+    region = "us-east-1"
+    key    = "borked.tfstate"
+  }
+}
+
+# API Gateway
+resource "aws_api_gateway_rest_api" "api" {
+  name = "myapi"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  http_method             = "${aws_api_gateway_method.method.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda.arn}/invocations"
+}
+
+# Lambda
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.lambda.arn}"
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}/resourcepath/subresourcepath"
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename         = "seed_handler.zip"
+  function_name    = "mylambda"
+  role             = "${aws_iam_role.role.arn}"
+  handler          = "lambda.lambda_handler"
+  runtime          = "python2.7"
+  source_code_hash = "${base64sha256(file("seed_handler.zip"))}"
+}
+
+# IAM
+resource "aws_iam_role" "role" {
+  name = "myrole"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
