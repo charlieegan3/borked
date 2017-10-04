9c642eaf129a276647e7cf589153611b16fa9b62
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-10-04 23:10:15 +0100

    Revert "Count requests and links processed"
    This reverts commit 76004715fcf72997d5b36b9ae3ca0c528d139a16.

--- dynamo.go
+++ dynamo.go
@@ -7,62 +7,12 @@ import (
 	"fmt"
 	"log"
 	"net/url"
-	"strconv"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
-
-func getCount(key string) (int, error) {
-	svc := dynamodb.New(session.New())
-
-	query := &dynamodb.GetItemInput{
-		Key: map[string]*dynamodb.AttributeValue{
-			"Count": {
-				S: aws.String(key),
-			},
-		},
-		TableName: aws.String("borked-counts"),
-	}
-
-	result, err := svc.GetItem(query)
-	if err != nil {
-		return 0, err
-	}
-
-	i, err := strconv.Atoi(*result.Item["Value"].N)
-	if err != nil {
-		return 0, err
-	}
-	return i, nil
-}
-
-func setCount(key string, value int) error {
-	svc := dynamodb.New(session.New())
-	input := &dynamodb.UpdateItemInput{
-		ExpressionAttributeNames: map[string]*string{
-			"#V": aws.String("Value"),
-		},
-		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-			":v": {
-				N: aws.String(fmt.Sprintf("%v", value)),
-			},
-		},
-		Key: map[string]*dynamodb.AttributeValue{
-			"Count": {
-				S: aws.String(key),
-			},
-		},
-		ReturnValues:     aws.String("ALL_NEW"),
-		TableName:        aws.String("borked-counts"),
-		UpdateExpression: aws.String("SET #V = :v"),
-	}
-
-	_, err := svc.UpdateItem(input)
-	return err
-}
 
 // LogJob saves the results of a job to DynamoDB
 func LogJob(rootURL url.URL, linkCount int, source string, userAgent string) {
@@ -98,22 +48,11 @@ func LogJob(rootURL url.URL, linkCount int, source string, userAgent string) {
 			},
 		},
 		ReturnConsumedCapacity: aws.String("TOTAL"),
-		TableName:              aws.String("borked-jobs"),
+		TableName:              aws.String("BorkedJobs"),
 	}
 
 	_, err := svc.PutItem(input)
 	if err != nil {
 		log.Println(err.Error())
-		return
 	}
-
-	completedCount, err := getCount("Completed")
-	requestsCount, err := getCount("Requests")
-	if err != nil {
-		log.Println(err.Error())
-		return
-	}
-
-	setCount("Completed", completedCount+linkCount)
-	setCount("Requests", requestsCount+1)
 }
--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,23 +1,11 @@
 resource "aws_dynamodb_table" "jobs" {
-  name           = "${var.project}-jobs"
+  name           = "BorkedJobs"
   read_capacity  = 20
   write_capacity = 20
   hash_key       = "JobId"
 
   attribute {
     name = "JobId"
-    type = "S"
-  }
-}
-
-resource "aws_dynamodb_table" "counts" {
-  name           = "${var.project}-counts"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "Count"
-
-  attribute {
-    name = "Count"
     type = "S"
   }
 }
